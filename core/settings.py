0959a92b1acee0749e85358452a3cd178f44b41a
Jonathan Paiva <jonathanapaiva@gmail.com>
2025-02-22 18:53:29 -0300

    feat: update settings

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#1tvttrm&++p)wa0jmvd_ss3vh!$++3)_rcr27o!&gml=)h8od
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
